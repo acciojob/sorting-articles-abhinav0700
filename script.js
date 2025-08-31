425b1aac7e008443fc0d5a231e2b1d4e501d057b
Abhinav M <130998983+abhinav0700@users.noreply.github.com>
2025-08-31 21:51:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 //your JS code here. If required.
+const bands = [
+      'The Plot in You',
+      'The Devil Wears Prada',
+      'Pierce the Veil',
+      'Norma Jean',
+      'The Bled',
+      'Say Anything',
+      'The Midway State',
+      'We Came as Romans',
+      'Counterparts',
+      'Oh, Sleeper',
+      'A Skylit Drive',
+      'Anywhere But Here',
+      'An Old Dog'
+    ];
+
+function stripp(inp) {
+	return inp.replace(/(a|an|the)/g,'').trim();	
+};
+
+cost sorted_bands = bands.sort((a,b)=>{
+	return stripp(a).localeCompare(stripp(b));
+});
+
+document.getElementById('band').innerHTML = sorted_band.map(band=>`<li>${band}</li>`).join('');
